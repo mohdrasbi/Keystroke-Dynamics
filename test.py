8e099915727927dac2d70c53a10d6a528f9cf850
mohdrasbi <mohdrasbi123@gmail.com>
2019-04-27 18:38:43 -0600

    Fix bugs

---
+++ test.py
@@ -0,0 +1,8 @@
+from functions import *
+
+raw_data = pd.read_csv("/Users/mohd/Desktop/Keystroke-Dynamics/data/user_0/raw_data/0.csv")
+
+print(raw_data)
+
+
+# final_data = ExtractFeatures(raw_data, user_id)
