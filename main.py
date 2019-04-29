fc736e1a638976baabff261f402041883b3b869f
Pranav Gummaraj Srinivas <48041393+prgu6170@users.noreply.github.com>
2019-04-29 00:17:03 -0600

    Added code to predict outcome using FitAndPredict
    Added additional checks before verifying a user.

--- main.py
+++ main.py
@@ -5,24 +5,32 @@ user = str(input("Enter username: "))
 user_ids = pd.read_csv("log/user_ids.csv")
 user_id = user_ids.shape[0]
 
-user_id_list = list(user_ids[user_ids['user'] == user]['id'])
-if len(user_id_list) > 0:
-	user_id = user_id_list[0]
-else:
-	user_ids.loc[user_id] = [user, user_id]
-	user_ids.to_csv("log/user_ids.csv", index=False)
+user_id_list = list(user_ids[user_ids['user'] == user.lower()]['id'])
 
 
-while(True):
-	print("Type:\n1 to collect training data\n2 to collect test data")
+while True:
+	print("Type:\n1 to collect training data\n2 to validate against existing data")
 	user_input = int(input("> "))
 
 	if user_input == 1:
+		if len(user_id_list) > 0:
+			user_id = user_id_list[0]
+		else:
+			user_ids.loc[user_id] = [user, user_id]
+			user_ids.to_csv("log/user_ids.csv", index=False)
 		raw_data = CollectData(user_id, "train_data").return_df()
 		final_data = ExtractFeatures(raw_data, user_id, "train_data")
 		break
 
 	elif user_input == 2:
-		raw_data = CollectData(user_id, "test_data").return_df()
-		final_data = ExtractFeatures(raw_data, user_id, "test_data")
+		if len(user_id_list) == 0:
+			print("The username does not exist")
+			continue
+		else:
+			user_id = user_id_list[0]
+			raw_data = CollectData(user_id, "test_data").return_df()
+			final_data = ExtractFeatures(raw_data, user_id, "test_data")
+			predict = FitAndPredict(user_id)
+			match = predict.SVM_classifier()
+			print('\n\n\n'+str(match) + '% match'+'\n\n\n')
 		break
